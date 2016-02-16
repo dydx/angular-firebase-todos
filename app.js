74f09c2d42cc2d8f7826d9224cb2334b3100461e
Joshua Sandlin <joshua.sandlin@gmail.com>
2016-02-16 12:19:30 -0500

    made a factory to wrap firebase

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 (function () {
   angular.module('todosApp', ['firebase']);
-  angular.module('todosApp')
-    .controller('todosCtrl', function ($firebaseArray) {
-      var ref = new Firebase('https://atl-wdi5-angular-todos.firebaseIO.com');
 
-      this.todos = $firebaseArray(ref);
+  angular.module('todosApp')
+    .factory('todosFactory', function ($firebaseArray) {
+
+      var ref = new Firebase('https://atl-wdi5-angular-todos.firebaseIO.com');
+      var todos = $firebaseArray(ref);
+
+      function add (todo) {
+        todos.$add(todo);
+      }
+
+      function remove (todo) {
+        todos.$remove(todo);
+      }
+
+      return {
+        add: add,
+        remove: remove,
+        todos: todos
+      };
+    });
+
+  angular.module('todosApp')
+    .controller('todosCtrl', function (todosFactory) {
+
+      this.todos = todosFactory.todos;
 
       this.newTodo = {
         title: '',
         completed: false
       };
 
-      // I'm intentionally wrapping the Firebase $add and $remove methods
-      // here instead of calling them directly in my view
       this.addTodo = function () {
-        this.todos.$add(angular.copy(this.newTodo));
+        todosFactory.add(angular.copy(this.newTodo));
       };
 
       this.finishTodo = function (todo) {
-        this.todos.$remove(todo);
+        todosFactory.remove(todo);
       };
 
     });
